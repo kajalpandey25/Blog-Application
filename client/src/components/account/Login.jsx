0dcad35912c5a75fd15b9a168090e38a86acdd35
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-11-12 16:42:51 +0530

    Ok

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,4 +1,4 @@
-import { Box, TextField, Button, styled } from "@mui/material";
+import { Box, TextField, Button, styled, Typography } from "@mui/material";
 
 const Component = styled(Box)`
   width: 400px;
@@ -6,24 +6,60 @@ const Component = styled(Box)`
   box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
 `;
 
-const Image = styled('img')({
-    width: 100,
-    margin: 'auto',
-    display: 'flex',
-    padding: '50px 0 0'
-})
+const Image = styled("img")({
+  width: 100,
+  margin: "auto",
+  display: "flex",
+  padding: "50px 0 0",
+});
+
+const Wrapper = styled(Box)`
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  & > div, & > button, & > p {
+    margin-top: 20px;
+  }
+`;
+
+const LoginButton = styled(Button)`
+text-transform: none;
+background: #F8641B;
+color: #fff;
+height: 48px;
+border-radius: 2px;
+`;
+
+const SignButton= styled(Button)`
+text-transform: none;
+background: #fff;
+color: #2874f0;
+height: 48px;
+border-radius: 2px;
+box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
+`;
+
+const Text = styled(Typography)`
+color: #878787;
+font-size: 16px;
+`
+
 
 const Login = () => {
   const imageURL =
     "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
   return (
     <Component>
-        <Box>
-      <Image src={imageURL} alt="login" />
-      <TextField variant="standard" />
-      <TextField variant="standard" />
-      <Button variant="contained">Login</Button>
-      <Button>Create an account</Button>
+      <Box>
+        <Image src={imageURL} alt="login" />
+        <Wrapper>
+          <TextField variant="standard" />
+          <TextField variant="standard" />
+          <LoginButton variant="contained">Login</LoginButton>
+           <Text style={{ textAlign: 'center'}}>OR</Text>
+          <SignButton>Create an account</SignButton>
+        </Wrapper>
       </Box>
     </Component>
   );
